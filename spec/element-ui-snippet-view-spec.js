e84b55ae4b2e04e060fe2d97017a18180e191437
wangrq <likegun94@gmail.com>
2017-08-04 11:02:21 +0800

    initial commit

---
+++ spec/element-ui-snippet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ElementUiSnippetView from '../lib/element-ui-snippet-view';
+
+describe('ElementUiSnippetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
